88315fc9eee8c2f3087974c881f4ae838a7d5fc8
yindaheng98 <yindaheng98@gmail.com>
2025-08-26 23:46:41 -0700

    torch is default

--- prpa/__main__.py
+++ prpa/__main__.py
@@ -13,7 +13,7 @@ parser.add_argument("--local", type=str, required=True, help="Index of locally r
 parser.add_argument("--reference", type=str, required=True, help="Index of reference image.")
 parser.add_argument("--warped", type=str, required=True, help="Index to save warped image.")
 parser.add_argument("--bordermode", type=str, default='grid_sample')
-parser.add_argument("--backend", type=str, default='taichi', choices=['torch', 'taichi', 'cuda'])
+parser.add_argument("--backend", type=str, default='torch', choices=['torch', 'taichi', 'cuda'])
 parser.add_argument("--kernel-size", type=int, default=16, help="Erosion sliding window radius.")
 parser.add_argument("--occluded-dilation-size", type=int, default=0, help="Dilation size for occluded mask when selecting source pixels.")
 parser.add_argument("--occlude-dilation-size", type=int, default=0, help="Dilation size for occlude mask when selecting source pixels.")
